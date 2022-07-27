c606693e99be6324d39707a55a48588164960d89
wedburst <jhonrawed@gmail.com>
2022-07-26 22:47:33 -0500

    cookie in the cart products

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,56 +1,94 @@
-import { ICartProduct } from 'interfaces';
-import React, { FC, useReducer } from 'react'
-import { CartContext, cartReducer } from './';
+import React, { FC, useEffect, useReducer } from "react";
+import Cookie from "js-cookie";
+
+import { ICartProduct } from "interfaces";
+import { CartContext, cartReducer } from "./";
 
 export interface CartState {
-     cart: ICartProduct[];
+  cart: ICartProduct[];
 }
 
 const CART_INITIALSTATE: CartState = {
-     cart: [],
-}
+//   cart: [],
+  cart: Cookie.get('cart') ? JSON.parse(Cookie.get('cart')!) : []
+};
 
-export const CartProvider:FC = ({children}: any) => {
+export const CartProvider: FC = ({ children }: any) => {
+  const [state, dispatch] = useReducer(cartReducer, CART_INITIALSTATE);
 
-const [state, dispatch] = useReducer(cartReducer, CART_INITIALSTATE);
+  // Efecto
+  useEffect(() => {
+    try {
+      const cookieProducts = Cookie.get("cart")
+        ? JSON.parse(Cookie.get("cart")!)
+        : [];
+      dispatch({
+        type: "[Cart] - LoadCart from cookies | storage",
+        payload: cookieProducts,
+      });
+    } catch (error) {
+      dispatch({
+        type: "[Cart] - LoadCart from cookies | storage",
+        payload: [],
+      });
+    }
+  }, []);
 
-const addProductToCart = ( product: ICartProduct) => {
-     // Nivel 1
-     // dispath({ type: '[Cart] - Add Product', payload: product})
+  useEffect(() => {
+    Cookie.set("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
-     // Nivel 2
-     // const productsInCart = state.cart.filter( p => p._id !== product._id && p.size !== product.size);
-     // dispatch({ type: '[Cart] - Update products in cart', payload: [...productsInCart, product] });
+  const addProductToCart = (product: ICartProduct) => {
+    // Nivel 1
+    // dispath({ type: '[Cart] - Add Product', payload: product})
 
-     // Nivel Final
-     const productInCart = state.cart.some( p => p._id === product._id);
-     if ( !productInCart ) return dispatch({ type: '[Cart] - Update products in cart', payload: [...state.cart, product]})
+    // Nivel 2
+    // const productsInCart = state.cart.filter( p => p._id !== product._id && p.size !== product.size);
+    // dispatch({ type: '[Cart] - Update products in cart', payload: [...productsInCart, product] });
 
-     const productWithDifferentSize = state.cart.some( p => p._id === product._id && p.size === product.size );
-     if ( !productWithDifferentSize ) return dispatch({ type: '[Cart] - Update products in cart', payload: [...state.cart, product]})
+    // Nivel Final
+    const productInCart = state.cart.some((p) => p._id === product._id);
+    if (!productInCart)
+      return dispatch({
+        type: "[Cart] - Update products in cart",
+        payload: [...state.cart, product],
+      });
 
-     // Acumular
-     const updateProducts = state.cart.map( p => {
-          if ( p._id !== product._id ) return p;
-          if ( p.size !== product.size ) return p;
+    const productWithDifferentSize = state.cart.some(
+      (p) => p._id === product._id && p.size === product.size
+    );
+    if (!productWithDifferentSize)
+      return dispatch({
+        type: "[Cart] - Update products in cart",
+        payload: [...state.cart, product],
+      });
 
-          // Actualizar la cantidad
-          p.quantity += product.quantity;
-          return p;
-     });
+    // Acumular
+    const updateProducts = state.cart.map((p) => {
+      if (p._id !== product._id) return p;
+      if (p.size !== product.size) return p;
 
-     dispatch({ type: '[Cart] - Update products in cart', payload: updateProducts});
+      // Actualizar la cantidad
+      p.quantity += product.quantity;
+      return p;
+    });
 
-}
+    dispatch({
+      type: "[Cart] - Update products in cart",
+      payload: updateProducts,
+    });
+  };
 
-return (
-    <CartContext.Provider value={{
-     ...state,
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
 
-     // Methods
-     addProductToCart,
-    }}>
-     {children}
+        // Methods
+        addProductToCart,
+      }}
+    >
+      {children}
     </CartContext.Provider>
-)
-}
+  );
+};
