ecd989e636fbfce73c3aabec265728441d571714
wedburst <jhonrawed@gmail.com>
2022-08-13 00:47:53 -0500

    make token JWT and installing

---
+++ utils/jwt.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const signToken = (_id: string, email: string) => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error("No hay semilla de JWT - Revisar variables de entorno");
+  }
+
+  return jwt.sign(
+    {
+      // payload
+      _id,
+      email,
+    },
+    // SEED
+    process.env.JWT_SECRET_SEED,
+
+    // Opciones
+    { expiresIn: "30d" }
+  );
+};
