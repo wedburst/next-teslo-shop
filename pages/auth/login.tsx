28aded7a49b089754939e652e5f6a08f3888f574
wedburst <jhonrawed@gmail.com>
2022-08-16 12:30:53 -0500

    installing and integration react-hook-form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,36 +1,72 @@
+import NextLink from "next/link";
+import { useForm } from "react-hook-form";
+
 import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "components/layouts";
-import NextLink from "next/link";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const loginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onLoginUser = (data: FormData) => {
+    console.log({ data });
+  };
+
   return (
     <AuthLayout title={"Login"}>
-      <Box sx={{ width: 350, padding: '10px 20px'}}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-              <Typography variant="h1" component="h1">Iniciar Sesión</Typography>
-          </Grid>
-          <Grid item xs={12}>
-              <TextField label="Correo" variant="filled" fullWidth/>
-          </Grid>
-          <Grid item xs={12}>
-              <TextField label="Contraseña" type="password" variant="filled" fullWidth/>
-          </Grid>
-          <Grid item xs={12}>
-              <Button color="secondary" className="circular-btn" size="large" fullWidth>
-                  Iniciar Sesión
+      <form onSubmit={handleSubmit(onLoginUser)}>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Iniciar Sesión
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register("email")}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register("password")}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Iniciar Sesión
               </Button>
-          </Grid>
+            </Grid>
 
-          <Grid item xs={12} display="flex" justifyContent="end">
+            <Grid item xs={12} display="flex" justifyContent="end">
               <NextLink href="/auth/register" passHref>
-                <Link underline="always">
-                    ¿No tienes cuenta?
-                </Link>
+                <Link underline="always">¿No tienes cuenta?</Link>
               </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
