fc9679f81150d2c75af9b56484cb4c7c93e59099
wedburst <jhonrawed@gmail.com>
2022-08-16 00:04:50 -0500

    validate token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -18,3 +18,25 @@ export const signToken = (_id: string, email: string) => {
     { expiresIn: "30d" }
   );
 };
+
+export const isValidToken = ( token: string ):Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error("No hay semilla de JWT - Revisar variables de entorno");
+  }
+
+  return new Promise( ( resolve, reject ) => {
+    try {
+      jwt.verify( token, process.env.JWT_SECRET_SEED || "", (err, payload) => {
+        if ( err ) return reject('JWT no es válido');
+
+        const { _id } = payload as { _id: string };
+
+        resolve(_id);
+
+      })
+    } catch (err) {
+      console.log(err)
+      reject('JWT no es válido');
+    }
+  } )
+}
