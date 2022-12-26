55d0e96a745e5e3ed309616d096e5c88574a41e2
wedburst <jhonrawed@gmail.com>
2022-12-26 00:35:37 -0500

    middleware problem

---
+++ pages/checkout/_middlewawre.ts
@@ -0,0 +1,21 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+// import { jwt } from 'utils';
+
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+ 
+    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+   
+    console.log({ session })
+   
+    if (!session) {
+      const url = req.nextUrl.clone()
+      const requestedPage = req.page.name
+      url.pathname = `/auth/login`
+      url.search = `?p=${requestedPage}`
+      return NextResponse.redirect(url)
+    }
+   
+    return NextResponse.next()
+  }
